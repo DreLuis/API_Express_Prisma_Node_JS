c806758270b5421470811607845ffc21a312e379
DreLuis <andre_santo@estudante.sc.senai.br>
2024-06-05 20:18:29 -0300

    tudo

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require("dotenv/config")
+
+// Index das routes
+const routes = require('./routes')
+
+
+const app = express()
+app.use(express.json())
+
+
+app.use(routes)
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
